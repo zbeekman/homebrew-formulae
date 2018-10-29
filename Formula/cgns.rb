f008117519f4e898322b712d18ef8a77aad320be
Izaak Beekman <zbeekman@gmail.com>
2018-10-29 14:47:26 -0400

    Import CGNS and hwloc with X11

---
+++ Formula/cgns.rb
@@ -0,0 +1,56 @@
+class Cgns < Formula
+  desc "CFD General Notation System"
+  homepage "http://cgns.org/"
+  url "https://github.com/CGNS/CGNS/archive/v3.3.1.tar.gz"
+  sha256 "81093693b2e21a99c5640b82b267a495625b663d7b8125d5f1e9e7aaa1f8d469"
+
+  depends_on "gcc"
+  depends_on "cmake" => :build
+  depends_on "hdf5" => :recommended
+  depends_on "szip"
+
+  def install
+    args = std_cmake_args + [
+      "-DCGNS_ENABLE_TESTS=YES",
+    ]
+
+    args << "-DCGNS_ENABLE_64BIT=YES" if Hardware::CPU.is_64_bit? && MacOS.version >= :snow_leopard
+    args << "-DCGNS_ENABLE_FORTRAN=YES"
+
+    if build.with? "hdf5"
+      args << "-DCGNS_ENABLE_HDF5=YES"
+      args << "-DHDF5_NEED_ZLIB=YES"
+      args << "-DHDF5_NEED_SLIB=YES"
+      args << "-DCMAKE_SHARED_LINKER_FLAGS=-lhdf5"
+    end
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make"
+      system "ctest", "--output-on-failure"
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <stdio.h>
+      #include "cgnslib.h"
+      int main(int argc, char *argv[])
+      {
+        int filetype = CG_FILE_NONE;
+        // we expect this to fail, as the test executable isn't a CGNS file
+        if (cg_is_cgns(argv[0], &filetype) != CG_ERROR)
+          return 1; // should fail!
+        printf(\"%d.%d.%d\\n\",CGNS_VERSION/1000,(CGNS_VERSION/100)%10,(CGNS_VERSION/10)%10);
+        return 0;
+      }
+    EOS
+    compiler = Tab.for_name("cgns").with?("hdf5") ? "h5cc" : ENV.cc
+    # The rpath to szip needs to be passed explicitely here because the
+    # compiler may be h5cc (Superenv is not supported in that case)
+    rpath = "-Wl,-rpath=#{Formula["szip"].opt_lib}" unless OS.mac?
+    system compiler, "-I#{opt_include}", testpath/"test.c", "-L#{opt_lib}", "-lcgns", *rpath
+    assert_match(/#{version}/, shell_output("./a.out"))
+  end
+end
---
+++ Formula/hwloc.rb
@@ -0,0 +1,42 @@
+class Hwloc < Formula
+  desc "Portable abstraction of the hierarchical topology of modern architectures"
+  homepage "https://www.open-mpi.org/projects/hwloc/"
+  url "https://www.open-mpi.org/software/hwloc/v2.0/downloads/hwloc-2.0.2.tar.bz2"
+  sha256 "14457d70e6f98ee9eb3f2940000da4bac99909a49560ef2fdf4eacd286410cde"
+
+  bottle do
+    cellar :any
+    sha256 "1bab0bc7648791a559811cdc7135f54879aa5992948529942502279bdcdde3cc" => :mojave
+    sha256 "f9d9eff8707c433e53322487dec416e7f55aecb115b33cdbd57a9e53d23898c2" => :high_sierra
+    sha256 "403bc0d7b05a1a371be6b28f6732ec0758d35789a257330fd96f01c1162518f8" => :sierra
+    sha256 "6cb2aca1177a2dea107e6edb32b9a4383de751a41290688b8fd1b4f55529fbac" => :el_capitan
+  end
+
+  head do
+    url "https://github.com/open-mpi/hwloc.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "cairo"
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--enable-shared",
+                          "--enable-static",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+
+    pkgshare.install "tests"
+  end
+
+  test do
+    system ENV.cc, pkgshare/"tests/hwloc/hwloc_groups.c", "-I#{include}",
+                   "-L#{lib}", "-lhwloc", "-o", "test"
+    system "./test"
+  end
+end
